a154cd2faab99e05fd32459276295f4b50eeb8b4
klaudia <klaudiapajor17@gmail.com>
2021-04-18 19:15:26 +0200

    wyszukiwarka miast

--- google/vue4.js
+++ google/vue4.js
@@ -1,6 +1,16 @@
 var app = new Vue({
     el: '#app', 
     data: {
-      googleSearch: ''
-    }
+      googleSearch: '',
+      cities: window.cities
+    },
+    computed: {
+      filteredCities: function() {
+          let m = this.cities.filter(city => city.name.includes(this.googleSearch))
+          if(m.length > 10){
+            return m.slice(0,11);
+          }
+          return m
+        }
+      }
   });
