64dd03a46845e7a32522731a20129bf8563be539
klaudia <klaudiapajor17@gmail.com>
2021-05-15 13:55:31 +0200

    popprawione

--- google/vue4.js
+++ google/vue4.js
@@ -1,64 +1,79 @@
 var app = new Vue({
-  el: '#app', 
-  data: 
-  {
-    googleSearch: '',
-    cities: window.cities,
-    isActive: 0,
-    kontrol: 0,
-    indeks: -1,
-    zmien_kolor: 1
-  },
-  computed: 
-  {
-    filteredCities: function() 
+    el: '#app', 
+    data: 
     {
-        let m = this.cities.filter(city => city.name.includes(this.googleSearch));
-        if(m.length > 10)
-        {
-          m = m.slice(0,10);
+      googleSearch: '',
+      isActive: 0,
+      kontrol: 0,
+      autocompleterIsActive: false,
+      activeResult: 0,
+      filteredCities: [],
+      cities: window.cities.map((cityData) => {
+        cityData.nameLowerCase = cityData.name.toLowerCase();
+        return cityData;
+      })
+    },
+    watch: 
+    {
+      googleSearch() {
+        if (this.autocompleterIsActive) {
+            return;
         }
-        return m;
-    }
-  },
-  methods:
-  {
-    zmiana: function(a)
-    {
-      if(this.isActive == 0)
-      {
-        this.isActive = 1;
-        this.googleSearch = a;
-        el2 = document.getElementById("autocom");
-        el2.blur();
-        this.kontrol = 0;
+        if (this.googleSearch.length === 0) {
+            filteredCities = [];
+            return;
+        }
+        let returnedCities = [];
+        let searchLowerCase = this.googleSearch.toLowerCase();
+
+        this.cities.forEach((cityData) => {
+            if (returnedCities.length === 10 || !cityData.nameLowerCase.includes(searchLowerCase)) {
+                return;
+            }
+            returnedCities.push({
+                name: cityData.name,
+                nameHtml: cityData.nameLowerCase.replace(searchLowerCase, (match) => {
+                    return '<span class="bold">' + match + '</span>';
+                })
+            })
+        });
+        
+        this.filteredCities = returnedCities;
       }
     },
-    pogrub: function(a)
+    methods:
     {
-      fraza = this.googleSearch;
-      var pom = a.split(fraza);
-      for(i = 0; i < pom.length; i++)
+      zmiana: function(a)
       {
-        a = a.replace(pom[i], pom[i].bold());
+        if(this.isActive == 0)
+        {
+          this.isActive = 1;
+          this.googleSearch = a;
+          el2 = document.getElementById("autocom");
+          el2.blur();
+          this.kontrol = 0;
+        }
+      },
+      ustaw: function()
+      {
+        this.kontrol = 1;
+      },
+      strzalka: function(index)
+      {
+        if (!this.autocompleterIsActive) {
+          index = 0;
+        } 
+
+        if (index > this.filteredCities.length - 1) {
+            index = 0;
+        } else if (index < 0) {
+            index = this.filteredCities.length - 1;
+        }
+        
+        this.autocompleterIsActive = true;
+        this.activeResult = index;
+        this.googleSearch = this.filteredCities[index].name;
       }
-      return a;
-    },
-    ustaw: function()
-    {
-      this.kontrol = 1;
-    },
-    strzalka_gora: function()
-    {
-      this.googleSearch = "strzalka w gore";
-    },
-    strzalka_dol: function()
-    {
-      this.indeks = this.indeks + 1;
-      let autocom = document.getElementsByClassName("resultsBox");
-      let dl = autocom.length;
-      autocom.childNodes[0];
-      this.zmien_kolor = this.zmien_kolor + 1;
     }
-  }
-});
+  
+  });
